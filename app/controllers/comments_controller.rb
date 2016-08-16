db3b7088df430290fe1d35fd9d95cb6819b42f7b
Olena Dykhno <olenadykhno@gmail.com>
2016-08-16 12:10:13 +0300

    add gem bootstrap in app

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,7 +14,7 @@ class CommentsController < ApplicationController
     @comment.destroy
     redirect_to post_path(@post)
   end
-  
+
   private
   def comment_params
     params.require(:comment).permit(:commenter, :body)
